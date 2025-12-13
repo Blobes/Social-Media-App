d87e6ec0d5697bd50672b03275ca4c730e27e689
Blobes <officialblobes@gmail.com>
2025-12-13 16:57:12 +0100

    Fixed

--- Client/src/app/timeline/Posts.tsx
+++ Client/src/app/timeline/Posts.tsx
@@ -19,7 +19,6 @@ export const Posts = () => {
   const [message, setMessage] = useState<string | null>(null);
   const { loginStatus } = useAppContext();
   const [isLoading, setLoading] = useState(false);
-  console.log("Hel");
 
   const renderPosts = async () => {
     try {
