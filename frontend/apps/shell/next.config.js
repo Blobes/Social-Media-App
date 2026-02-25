59f2c337eabba2a6fc1de1cb73b42934ae542fb3
Blobes <officialblobes@gmail.com>
2026-02-26 00:09:28 +0100

    refactor: update backend API URL in next.config.js to a hardcoded value

--- frontend/apps/shell/next.config.js
+++ frontend/apps/shell/next.config.js
@@ -3,4 +3,4 @@ import { withCommonConfig } from "@repo/env-config";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withCommonConfig(nextConfig, process.env.NEXT_PUBLIC_API_URL);
+export default withCommonConfig(nextConfig, "https://funstakes.onrender.com");
