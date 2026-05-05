e99ae5be1fe1fd0c6d9cf6a90946200b49c33cba
Blobes <officialblobes@gmail.com>
2026-05-05 19:56:12 +0100

    Fixed

--- backend-coolify/packages/shared/env-config/dotenvConfig.ts
+++ backend-coolify/packages/shared/env-config/dotenvConfig.ts
@@ -1,31 +1,9 @@
 import dotenv from "dotenv";
 import path from "path";
 
-/**
- * Automatically loads the correct .env file from the monorepo root.
- * We use a relative path logic that knows shared is in packages/shared
- * and needs to look 2 levels up to find the root.
- */
-// export const initEnv = (env: string) => {
-//   const envMode = env || "development";
-//   const envFile = `.env.${envMode}`;
-//   const envPath = path.resolve(process.cwd(), "../../", envFile);
-
-//   if (fs.existsSync(envPath)) {
-//     dotenv.config({ path: envPath });
-//     // // Explicitly set the process variable so the rest of the app sees it
-//     console.log(`📡 Environment initialized: ${envFile}`);
-//   } else {
-//     console.error(`❌ Critical: Could not find ${envFile}`);
-//   }
-// };
-
 export function loadEnv(serviceName: string) {
   const rootEnvPath = path.resolve(process.cwd(), "../../.env.development");
-  const serviceEnvPath = path.resolve(
-    process.cwd(),
-    `../../secrets/.env.${serviceName}`,
-  );
+  const serviceEnvPath = path.resolve(process.cwd(), "../../.env.production");
 
   // 1. Load base env (non-sensitive defaults)
   const rootResult = dotenv.config({ path: rootEnvPath });
@@ -39,7 +17,7 @@ export function loadEnv(serviceName: string) {
   // 2. Load service-specific secrets (OVERRIDES root)
   const serviceResult = dotenv.config({
     path: serviceEnvPath,
-    override: true, // 🔥 THIS IS THE KEY
+    override: false, // 🔥 THIS IS THE KEY
   });
 
   if (serviceResult.error) {
