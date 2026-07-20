e1bc5575edd393a89498b83c6c5f18a5bed02714
Blobes <officialblobes@gmail.com>
2026-07-20 22:15:07 +0100

    Fix

--- backend-coolify/packages/security/src/index.ts
+++ backend-coolify/packages/security/src/index.ts
@@ -3,9 +3,6 @@ export * from "./auth/middlewares/userAuth";
 export * from "./auth/middlewares/internalAuth";
 
 export * from "./authorization/middlewares/isAdmin";
-export * from "./authorization/constants/permissions";
-export * from "./authorization/constants/roles";
-export * from "./authorization/types";
 
 export * from "./audit/events/middleware";
 export * from "./audit/events/service";
